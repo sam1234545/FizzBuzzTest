2ab0343571b748f58799262ebfdc1a700b288b90
Samah NABIH <samah.nabih@arolla.fr>
2022-06-20 14:56:29 +0200

    Add test for 2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -20,8 +20,20 @@ namespace FizzBuzzTest
             Assert.AreEqual(1, actual);
         }
 
+        [Test]
+        public void GivenInput_2_return2()
+        {
+            //arrange
+            int value = 2;
+            //act
+            var actual = FizzBuzzTest(value);
+            //assert
+            Assert.AreEqual(2, actual);
+        }
+
         private object FizzBuzzTest(int value)
         {
+            if (value == 2) return 2;
             return 1;
         }
     }
